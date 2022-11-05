9ecc96a73d2900783f57145f1bdde05a7f69c6a0
kumkum <kumkumrathour00@gmail.com>
2022-11-05 14:34:53 +0530

    numberOfDight

--- Basic/Introduction.java
+++ Basic/Introduction.java
@@ -1,3 +1,13 @@
 // analysis of algorithm study ever 
 // vedio 3 
 // asymptotic notation study .ever
+// question 1 uit 2 mathmatics
+/* int countdigit(int x){
+    count =0 ;
+    while(n>0){
+        n=n%10;
+        count++;
+    }
+    return count ;
+} 
+time com = thita d*/
